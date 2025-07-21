0d93a049728835289426cca11b580d2ec9313b8f
mothmoon <768.jac@gmail.com>
2025-07-21 19:03:28 +0200

    remove network-adj

--- post-build.sh
+++ post-build.sh
@@ -35,15 +35,6 @@ for script in $SCRIPTS; do
 	mv $script ${TARGET_INIT_D}/background/
 done
 
-# Add AP config line to iwd init script
-if [ -e ${TARGET_DIR}/etc/init.d/background/S40iwd ]; then
-	grep -qE '^mkdir -p /var/lib/iwd' ${TARGET_DIR}/etc/init.d/background/S40iwd || \
-		sed -i '/printf "Starting iwd:"/a\
-mkdir -p /var/lib/iwd; cp /boot/wlan/*.psk /var/lib/iwd/ 2>/dev/null || :' \
-			${TARGET_DIR}/etc/init.d/background/S40iwd
-fi
-
-
 # Create mount points and update fstab
 mkdir -p ${TARGET_DIR}/boot
 grep -qE "^/dev/mmcblk0p1  /boot" ${TARGET_DIR}/etc/fstab \
@@ -58,24 +49,6 @@ if grep -qE '^# set convert-meta off$' ${TARGET_DIR}/etc/inputrc; then
 elif ! grep -qE '^set convert-meta off$' ${TARGET_DIR}/etc/inputrc; then
 	echo 'set convert-meta off' >> ${TARGET_DIR}/etc/inputrc
 fi
-
-# Pip requires source files to be available, Buildroot only installs pyc
-for python_dir in ${TARGET_DIR}/usr/lib/python3.*; do
-	if [ -d $python_dir/site-packages/pip ]; then
-
-		for pip_dir in ${BUILD_DIR}/python-pip-*; do
-			if [ -d $pip_dir/src/pip ]; then
-				pushd $pip_dir/src/pip
-				find . -type f -name \*.py -exec \
-					install -D {} $python_dir/site-packages/pip/{} \;
-				popd
-			fi
-		done
-	fi
-done
-
-# Point DNS entries in resolv.conf to file managed by NetworkManager
-ln -sf ../run/NetworkManager/resolv.conf ${TARGET_DIR}/etc/resolv.conf
 
 # Reduce verbosity of kernel messages printed to the terminal
 echo 'kernel.printk = 3 4 1 3' >> ${TARGET_DIR}/etc/sysctl.conf
